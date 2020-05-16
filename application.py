66ea0718020fc929493f670167f47adfa6927cdc
nofilter4ever <65107045+nofilter4ever@users.noreply.github.com>
2020-05-16 16:49:49 +0800

    reset twice

--- application.py
+++ application.py
@@ -13,5 +13,10 @@ def shop():
     return render_template('shopindex.html')
 
 
+@app.route('/shop')
+def shop2():
+    return render_template('shop.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
