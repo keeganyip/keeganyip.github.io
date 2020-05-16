4f6da02d1451843f76f2f25548cb1e36723fa7a2
nofilter4ever <65107045+nofilter4ever@users.noreply.github.com>
2020-05-16 16:16:43 +0800

    added shop.html

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, Markup
 
+
 app = Flask(__name__)
 
 
@@ -12,6 +13,10 @@ def home():
 def shop():
     return render_template('shopindex.html')
 
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
+
